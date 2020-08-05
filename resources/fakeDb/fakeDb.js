695782b6a08a929483ead01279940c450774141e
Shahar Yigal <54366111+shahar16@users.noreply.github.com>
2020-08-05 19:43:33 +0300

    Add fake db infrastructure (#2)

---
+++ resources/fakeDb/fakeDb.js
@@ -0,0 +1,16 @@
+/**
+ * This factory exports a fakeDb in object.
+ * fakeDb["users"] : array of users
+ * fakeDb["products"] : array of products
+ * fakeDb["stores"] : array of stores
+ * */
+const usersDb = require( "./users.fakeDb" );
+const productsDb = require( "./products.fakeDb" );
+const storesDb = require( "./stores.fakeDb" );
+const fakeDb = {}
+
+fakeDb["users"] = usersDb;
+fakeDb["products"] = productsDb;
+fakeDb["stores"] = storesDb;
+
+module.exports = fakeDb;
