40d192b4b1f3c324c6ba1af8e3482457bceed97b
ahDaniel <49025216+ahDaniel@users.noreply.github.com>
2020-08-30 21:47:22 +0300

    Update cartProduct.model.js

--- models/cartProduct.model.js
+++ models/cartProduct.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-const productSchema = new Schema(
+const cartProductSchema = new Schema(
     {
         id: {
             type: String,
