f1f181b8fd4863a108d4e6fe04fc05f2055e07f9
ahDaniel <49025216+ahDaniel@users.noreply.github.com>
2020-08-31 20:00:12 +0300

    order func (#16)

--- routes/users.rout.js
+++ routes/users.rout.js
@@ -2,6 +2,7 @@ const express = require('express');
 const userController = require('../controllers/users.controller');
 const cartController = require('../controllers/cart.controller');
 const storeController = require('../controllers/store.controller');
+const orderController = require('../controllers/order.controller');
 
 const router = express.Router();
 const isAuth = require("../middlewares/isAuth");
@@ -12,11 +13,21 @@ router.post('/register', userController.register);
 router.post('/seedUsers', isAuth, userController.seedUsers);//router.post('/deleteUsers', isAuth, controller.removeAllUsersFromDb);
 router.post('/deleteUsers', userController.removeAllUsersFromDb);
 router.post('/addDbUsers', userController.addDbUsers);
+
+/**
+ * cart routes
+ */
 router.post('/addtocart', isAuth, cartController.addToCart, storeController.updateProduct);
 router.post('/removefromcart', isAuth, cartController.removeFromCart, storeController.updateProduct);
 router.post('/editcartitems', isAuth, cartController.editCartItems, storeController.updateProduct);
 router.get('/getcart', isAuth, cartController.getCart);
 router.post('/deletedbcarts', cartController.deleteDBCartItems);
+
+/**
+ * order routes
+ */
+router.post('/placeorder', isAuth, orderController.placeOrder);
+router.get('/getorderssummary', isAuth, orderController.getOrdersSummary);
 
 //TODO: deleteuser -> deleteStore -> deleteProductsInStore
 
