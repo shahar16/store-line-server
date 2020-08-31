11443847a48a5bedd7df1dd4524dba04353060ac
ahDaniel <danielahrak@gmail.com>
2020-08-31 19:52:29 +0300

    order func

--- models/cart.model.js
+++ models/cart.model.js
@@ -14,7 +14,28 @@ const cartSchema = new Schema(
         totalSum: {
             type: String,
             required: false
+        },
+        email: {
+            type: String,
+            required: false
+        },
+        shippingAddress: {
+            type: Object,
+            required: false
+        },
+        paymentMethod: {
+            type: Object,
+            required: false
+        },
+        orderDate: {
+            type: Date,
+            required: false
+        },
+        payed: {
+            type: Boolean,
+            required: false
         }
+
     },
     { timestamps: true }
 );
