779f0ce6391937ab4c6cca17fe5d4cb22adce1f1
Shahar Yigal <shahar.yigal@sap.com>
2020-09-11 13:16:16 +0300

    Add env vars

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,14 @@ const app = require('../app')
 
 const sPort = process.env.PORT || 8080
 
-const mongoAtlasUri = 'mongodb+srv://shaharyig:shyi9636963@projects-ple4w.mongodb.net/online_shop?retryWrites=true&w=majority'
+const mongoAtlasUri =
+        'mongodb+srv://' +
+        process.env.MONGO_USER +
+        ':' +
+        process.env.MONGO_PASSWORD +
+        '@projects-ple4w.mongodb.net/' +
+        process.env.MONGO_DEFULT_DB +
+        '?retryWrites=true&w=majority'
 
 mongoose
   .connect(mongoAtlasUri)
